7ebd32e368f61eeffbb847f6bd93925c477e19d0
Gaikwad Kunal Ganpat <162322840+KunalG932@users.noreply.github.com>
2024-05-01 01:08:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,53 +17,63 @@ photo_captions = {}
 
 @app.on_message(filters.photo & filters.private)
 def ask_for_caption(client, message):
-    # Save the chat ID to associate with the photo caption
-    photo_captions[message.chat.id] = {}
+    # Save the chat ID to associate with the photo captions
+    photo_captions[message.chat.id] = {'photo_ids': [], 'caption': None}
+
+    # Save the photo IDs
+    for photo in message.photo:
+        photo_id = photo.file_id
+        photo_captions[message.chat.id]['photo_ids'].append(photo_id)
 
     # Send a message asking for a caption
-    client.send_message(message.chat.id, "Send me a caption for the photo and document.")
-
-    # Save the photo ID
-    photo_id = message.photo.file_id
-    photo_captions[message.chat.id]['photo_id'] = photo_id
+    client.send_message(message.chat.id, "Send me a caption for the photos.")
 
 @app.on_message(filters.text & filters.private)
 def handle_caption(client, message):
     chat_id = message.chat.id
     if chat_id in photo_captions:
-        if 'photo_id' in photo_captions[chat_id]:
-            # Associate the caption with the photo ID
+        if photo_captions[chat_id]['caption'] is None:
+            # Associate the caption with the chat ID
             photo_captions[chat_id]['caption'] = message.text
 
-            # Forward the photo and document to the channel with the provided caption
-            forward_photo_and_document(client, chat_id)
+            # Forward the photos and documents to the channel with the provided caption
+            forward_photos_and_documents(client, chat_id)
 
             # Clean up the dictionary entry
             del photo_captions[chat_id]
 
-def forward_photo_and_document(client, chat_id):
+def forward_photos_and_documents(client, chat_id):
     channel_id = 'NanoSTestingArea'
-    caption = f"{photo_captions[chat_id]['caption']}"
+    caption = f"Forwarded from user: @{app.get_me().username}\n\n{photo_captions[chat_id]['caption']}"
 
-    # Get photo ID and caption from the dictionary
-    photo_id = photo_captions[chat_id]['photo_id']
+    # Get photo IDs and caption from the dictionary
+    photo_ids = photo_captions[chat_id]['photo_ids']
 
-    # Download photo to a temporary file
-    photo_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
-    app.download_media(photo_id, file_name=photo_path)
+    # Download photos to temporary files
+    photo_paths = []
+    for photo_id in photo_ids:
+        photo_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
+        app.download_media(photo_id, file_name=photo_path)
+        photo_paths.append(photo_path)
 
-    # Send photo to channel
-    with open(photo_path, "rb") as photo_file:
-        client.send_photo(channel_id, photo=photo_file, caption=caption)
+    # Send group media to channel
+    media_group = []
+    for photo_path in photo_paths:
+        with open(photo_path, "rb") as photo_file:
+            media_group.append({"type": "photo", "media": photo_file})
+    client.send_media_group(channel_id, media=media_group, caption=caption)
 
-    time.sleep(5)
+    # Wait for a short time to avoid rate limits
+    time.sleep(2)
 
-    # Send document to channel
-    with open(photo_path, "rb") as document_file:
-        client.send_document(channel_id, document=document_file, caption=caption)
+    # Send documents to channel one by one
+    for photo_path in photo_paths:
+        with open(photo_path, "rb") as document_file:
+            client.send_document(channel_id, document=document_file, caption=caption)
 
-    # Clean up: delete the temporary photo file
-    os.remove(photo_path)
+    # Clean up: delete the temporary photo files
+    for photo_path in photo_paths:
+        os.remove(photo_path)
 
 # Start the bot
 app.run()
