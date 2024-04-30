531c04ce512e71991365ba5cc0fa7cbc100808a1
Gaikwad Kunal Ganpat <162322840+KunalG932@users.noreply.github.com>
2024-05-01 00:49:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import time
 
 # Initialize your Pyrogram client
+# tialize your Pyrogram client
 api_id = 24496790
 api_hash = '95a711fc46d4293b7b419b9b6389b703'
 bot_token = '6728814239:AAFlWCe4xs9Yt8BoZjGUMgxRkMBdlZglndc'
@@ -13,32 +14,30 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 @app.on_message(filters.photo & filters.private)
 def forward_photo_to_channel(client, message):
     # Replace 'YOUR_CHANNEL_ID' with your channel ID
-    channel_id = '-1001905486162'
+    channel_id = '-1001234567890'  # Replace with your channel ID
 
     # Forward the photo to the channel as a photo with a caption
     caption = "Forwarded from user: @" + message.chat.username
-    photo_path = f"temp_photo_{time.time()}.jpg"
     
-    try:
-        # Download the photo
-        message.download(file_name=photo_path)
-        
-        # Open the photo file and send it as a photo
-        with open(photo_path, "rb") as photo_file:
-            client.send_photo(channel_id, photo=photo_file, caption=caption)
+    # Get the unique file ID of the photo
+    photo_id = message.photo.file_id
+    
+    # Save the photo in a temporary location using the file ID as the filename
+    photo_path = f"temp_photo_{photo_id}.jpg"
+    message.download(file_name=photo_path)
 
-        # Wait for 5 seconds
-        time.sleep(5)
+    # Send the photo to the channel
+    with open(photo_path, "rb") as photo_file:
+        client.send_photo(channel_id, photo=photo_file, caption=caption)
 
-        # Send the photo as a document to the channel
-        client.send_document(channel_id, document=photo_path, caption=caption)
-        
-    except FileNotFoundError:
-        print("Error: Photo file not found")
-    finally:
-        # Clean up: delete the temporary photo file
-        if os.path.exists(photo_path):
-            os.remove(photo_path)
+    # Wait for 5 seconds
+    time.sleep(5)
+
+    # Send the photo as a document to the channel
+    client.send_document(channel_id, document=photo_path, caption=caption)
+
+    # Clean up: delete the temporary photo file
+    os.remove(photo_path)
 
 # Start the bot
 app.run()
