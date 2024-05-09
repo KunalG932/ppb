e1ea48e1e593f7c52d0d7027f56e83d1a69779fd
Gaikwad Kunal Ganpat <162322840+KunalG932@users.noreply.github.com>
2024-05-09 12:52:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,14 +18,19 @@ photo_captions = {}
 @app.on_message(filters.photo & filters.private)
 def ask_for_caption(client, message):
     # Save the chat ID to associate with the photo captions
-    photo_captions[message.chat.id] = {'photo_ids': [], 'caption': None}
+    chat_id = message.chat.id
+    photo_captions[chat_id] = {'photo_ids': [], 'caption': None}
 
     # Save the photo ID
-    photo_id = message.photo.get_biggest().file_id
-    photo_captions[message.chat.id]['photo_ids'].append(photo_id)
+    photo_id = message.photo.file_id
+    photo_captions[chat_id]['photo_ids'].append(photo_id)
+
+    # Save the photo to a folder
+    photo_file_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
+    message.download(file_name=photo_file_path)
 
     # Send a message asking for a caption
-    client.send_message(message.chat.id, "Send me a caption for the photos. Please provide two texts separated by '|'.")
+    client.send_message(chat_id, "Send me a caption for the photos. Please provide two texts separated by '|'.")
 
 @app.on_message(filters.text & filters.private)
 def handle_caption(client, message):
@@ -41,8 +46,14 @@ def handle_caption(client, message):
                 # Forward the photos to the channel with the provided caption
                 forward_photos(client, chat_id)
 
-                # Clean up the dictionary entry
+                # Clean up the dictionary entry and delete the photo file
                 del photo_captions[chat_id]
+                photo_ids = photo_captions[chat_id]['photo_ids']
+                for photo_id in photo_ids:
+                    photo_file_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
+                    os.remove(photo_file_path)
+                del photo_captions[chat_id]
+
             else:
                 # If the format is incorrect, ask the user to resend the caption
                 client.send_message(chat_id, "Please provide the caption in the correct format. Two texts separated by '|'.")
@@ -54,21 +65,16 @@ def forward_photos(client, chat_id):
     # Get photo IDs and caption from the dictionary
     photo_ids = photo_captions[chat_id]['photo_ids']
 
-    # Download photos to temporary files
-    photo_paths = []
-    for photo_id in photo_ids:
-        photo_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
-        app.download_media(message=photo_id, file_name=photo_path)
-        photo_paths.append(photo_path)
-
     # Send photos to channel with the caption
-    for photo_path in photo_paths:
-        with open(photo_path, "rb") as photo_file:
+    for photo_id in photo_ids:
+        photo_file_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
+        with open(photo_file_path, "rb") as photo_file:
             client.send_photo(channel_id, photo=photo_file, caption=caption)
 
     # Clean up: delete the temporary photo files
-    for photo_path in photo_paths:
-        os.remove(photo_path)
+    for photo_id in photo_ids:
+        photo_file_path = os.path.join(photo_folder, f"temp_photo_{photo_id}.jpg")
+        os.remove(photo_file_path)
 
 # Start the bot
 app.run()
